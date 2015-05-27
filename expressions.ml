8d557cf47b82841f1faa03c276fe565662a70dc4
danshapero <shapero.daniel@gmail.com>
2015-05-26 23:45:24 -0700

    Function types have explicit return so that type checker needn't be invoked too early

--- expressions.ml
+++ expressions.ml
@@ -10,13 +10,13 @@ type constant =
 type data_type = Bool_t
                | Int_t
                | Float_t
-               | Function_t of data_type list
+               | Function_t of (data_type list) * data_type
 
 type expr =
     | Constant of constant
     | Variable of variable
     | Application of expr * expr list
-    | Abstraction of (variable * data_type) list * expr
+    | Abstraction of (variable * data_type) list * data_type * expr
     | Let of (variable * data_type * expr) list * expr
     | Letrec of (variable * data_type * expr) list * expr
     | Conditional of (expr * expr * expr)
@@ -31,11 +31,16 @@ let string_of_constant c =
 
 let rec string_of_data_type t =
   match t with
-    | Bool_t        -> "bool"
-    | Int_t         -> "int"
-    | Float_t       -> "float"
-    | Function_t ts -> let arg_strings = List.map string_of_data_type ts in
-                       "(-> " ^ (String.concat " " arg_strings) ^ ")"
+    | Bool_t                 -> "bool"
+    | Int_t                  -> "int"
+    | Float_t                -> "float"
+    | Function_t (args, ret) ->
+       let args_string =
+         String.concat " " (List.map string_of_data_type args)
+       and ret_string =
+         string_of_data_type ret
+       in
+       "(-> " ^ args_string ^ " " ^ ret_string ^ ")"
 
 
 let rec string_of_expr expr =
@@ -46,20 +51,28 @@ let rec string_of_expr expr =
     name ^ ":" ^ (string_of_data_type ty)
   in
   match expr with
-    | Constant c            -> string_of_constant c
-    | Variable v            -> v
-    | Application (f, args) -> let sf = string_of_expr f in
-                               let sa = List.map string_of_expr args in
-                               "(" ^ sf ^ " " ^ (String.concat " " sa) ^ ")"
-    | Abstraction (args, e) -> let s = string_of_expr e in
-                               let a = List.map string_of_arg_decl args in
-                               "(lambda (" ^ (String.concat " " a) ^ ") " ^ s ^ ")"
-    | Let (bindings, e)     -> let s = string_of_expr e in
-                               let b = List.map string_of_binding bindings in
-                               "(let (" ^ (String.concat " " b) ^ ") " ^ s ^ ")"
-    | Letrec (bindings, e)  -> let s = string_of_expr e in
-                               let b = List.map string_of_binding bindings in
-                               "(letrec (" ^ (String.concat " " b) ^ ") " ^ s ^ ")"
-    | Conditional (cond, t, f)       -> "(if " ^ (string_of_expr cond)
-                                        ^ " " ^ (string_of_expr t)
-                                        ^ " " ^ (string_of_expr f) ^ ")"
+    | Constant c ->
+       string_of_constant c
+    | Variable v ->
+       v
+    | Application (f, args) ->
+       let func = string_of_expr f
+       and args = List.map string_of_expr args in
+       "(" ^ func ^ " " ^ (String.concat " " args) ^ ")"
+    | Abstraction (args, ret, body) ->
+       let args = List.map string_of_arg_decl args
+       and ret  = string_of_data_type ret
+       and body = string_of_expr body in
+       "(lambda (" ^ (String.concat " " args) ^ "):" ^ ret ^ " " ^ body ^ ")"
+    | Let (bindings, body) ->
+       let bindings = List.map string_of_binding bindings
+       and body     = string_of_expr body in
+       "(let (" ^ (String.concat " " bindings) ^ ") " ^ body ^ ")"
+    | Letrec (bindings, body) ->
+       let bindings = List.map string_of_binding bindings
+       and body     = string_of_expr body in
+       "(letrec (" ^ (String.concat " " bindings) ^ ") " ^ body ^ ")"
+    | Conditional (cond, t, f) ->
+       "(if " ^ (string_of_expr cond) ^ " "
+       ^ (string_of_expr t) ^ " "
+       ^ (string_of_expr f) ^ ")"
