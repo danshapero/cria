c4686b321eb89de42170abf6f8e1828be1a6c7b1
danshapero <shapero.daniel@gmail.com>
2015-05-26 17:42:01 -0700

    More parser testing

--- testParser.ml
+++ testParser.ml
@@ -37,8 +37,59 @@ let correct_parse string_expression correct_expression =
 let test_fixture = "Expression parser" >:::
 [
   "atoms" >::
-    ( fun () -> assert_equal true (correct_parse "1" (Constant (Int 1)))
-    )
+    ( fun () -> assert_equal true
+                             (correct_parse "1" (Constant (Int 1)));
+                assert_equal true
+                             (correct_parse "1.0" (Constant (Float 1.0)));
+                assert_equal true
+                             (correct_parse "4.0e9" (Constant (Float 4.0e9)));
+                assert_equal true
+                             (correct_parse "4.0E9" (Constant (Float 4.0e9)));
+                assert_equal true
+                             (correct_parse "true" (Constant (Bool true)));
+                assert_equal true
+                             (correct_parse "false" (Constant (Bool false)))
+    );
+
+  "variables" >::
+    (
+      fun () -> assert_equal true
+                             (correct_parse "x" (Variable "x"))
+    );
+
+  "applications" >::
+    (
+      ( fun () ->
+        assert_equal true
+                     (correct_parse "(f 0)"
+                                    (Application (Variable "f",
+                                                  [Constant (Int 0)])));
+        assert_equal true
+                     (correct_parse "(g x y 0)"
+                                    (Application (Variable "g",
+                                                  [Variable "x";
+                                                   Variable "y";
+                                                   Constant (Int 0)])));
+        assert_equal false
+                     (correct_parse "(if b 0 1)"
+                                    (Application (Variable "if",
+                                                  [Variable "b";
+                                                   Constant (Int 0);
+                                                   Constant (Int 1)])))
+      )
+    );
+
+  "conditionals" >::
+    (
+      ( fun () ->
+        assert_equal true
+                     (correct_parse "(if b 0 1)"
+                                    (Conditional (Variable "b",
+                                                  Constant (Int 0),
+                                                  Constant (Int 1))))
+      )
+    );
+
 ]
 
 let _ =
