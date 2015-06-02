cde25bdec2566eb1960eb4461ad342767a47c9ac
danshapero <shapero.daniel@gmail.com>
2015-06-02 13:13:01 -0700

    Replaced manual entry of AST in testTypeChecker with parsing the expression from a string

--- testTypeChecker.ml
+++ testTypeChecker.ml
@@ -1,76 +1,68 @@
 
 open OUnit
 open Expressions
+open Lexer
+open Lexing
 open TypeChecker
+
+exception ParseFail of string;;
+
+let parse s =
+  let lexbuf = Lexing.from_string s in
+  let e = Parser.prog Lexer.read lexbuf in
+  match e with
+  | Some e -> e
+  | None   -> raise (ParseFail "Parsing expression failed!")
 
 let test_fixture = "TypeChecker" >:::
 [
   "constants" >::
     ( fun () ->
-      assert_equal Int_t (typeof (Constant (Int 1)) empty_context);
-      assert_equal Float_t (typeof (Constant (Float 1.0)) empty_context);
-      assert_equal Bool_t (typeof (Constant (Bool true)) empty_context);
-      assert_equal Bool_t (typeof (Constant (Bool false)) empty_context)
+      assert_equal Int_t (typeof (parse "1") empty_context);
+      assert_equal Float_t (typeof (parse "1.0") empty_context);
+      assert_equal Bool_t (typeof (parse "true") empty_context);
+      assert_equal Bool_t (typeof (parse "false") empty_context)
     );
 
   "abstractions" >::
     ( fun () ->
       assert_equal (Function_t ([Int_t], Int_t))
-                   (typeof (Abstraction ([("x", Int_t)],
-                                         Int_t,
-                                         Variable "x"))
+                   (typeof (parse "(lambda (x:int):int x)")
                            empty_context);
       assert_equal (Function_t ([Int_t; Int_t], Int_t))
-                   (typeof (Abstraction ([("x", Int_t);
-                                          ("y", Int_t)],
-                                         Int_t,
-                                         Application (Variable "+",
-                                                      [Variable "x";
-                                                       Variable "y"])))
-                            default_context)
+                   (typeof (parse "(lambda (x:int y:int):int (+ x y))")
+                           default_context)
     );
 
   "applications" >::
     ( fun () ->
       assert_equal Int_t
-                   (typeof (Application (Variable "+",
-                                         [Constant (Int 1);
-                                          Constant (Int 2)]))
-                   default_context)
-
+                   (typeof (parse "(+ 1 2)") default_context);
+      assert_equal Bool_t
+                   (typeof (parse "(> 4 2)") default_context);
+      assert_equal Int_t
+                   (typeof (parse "((lambda (x:int):int (+ x 1)) 1)")
+                           default_context)
     );
 
   "let" >::
     ( fun () ->
       assert_equal Int_t
-                   (typeof (Let ([("x", Int_t, Constant (Int 1))],
-                                 (Application (Variable "+",
-                                               [Variable "x";
-                                                Constant (Int 1)]))))
-                           default_context)
-
+                   (typeof (parse "(let (x:int 1) (+ x 1))")
+                           default_context);
+      assert_equal (Function_t ([Int_t], Int_t))
+                   (typeof
+                      (parse "(let (x:int 42) (lambda (k:int):int (* k x)))")
+                      default_context)
     );
 
   "conditionals" >::
     ( fun () ->
       assert_equal Int_t
-                   (typeof (Conditional (Constant (Bool true),
-                                         Constant (Int 1),
-                                         Constant (Int 2)))
-                           empty_context);
+                   (typeof (parse "(if true 1 2)") empty_context);
       assert_equal (Function_t ([Int_t], Int_t))
                    (typeof
-                      (Abstraction
-                         ([("k", Int_t)],
-                          Int_t,
-                          Application (Variable "*",
-                                       [Variable "k";
-                                        Conditional (Application
-                                                       (Variable ">",
-                                                        [Variable "k";
-                                                         Constant (Int 0)]),
-                                                     Constant (Int 1),
-                                                     Constant (Int ~-1))])))
+                      (parse "(lambda (x:int):int (* (if (> x 0) 1 -1) x))")
                       default_context)
     );
 ]
