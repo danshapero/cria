21189db450212cff09150564aa4f33d30bb6189d
danshapero <shapero.daniel@gmail.com>
2015-06-06 09:37:00 -0700

    Added a function to print integers to default type context

--- defaultTypeContext.ml
+++ defaultTypeContext.ml
@@ -6,12 +6,17 @@ open TypeChecker
 let iarithmetic = ["+"; "-"; "*"; "/"; "^"; "%"]
 let icomparison = ["<"; ">"; ">="; "<="; "="]
 let logical_binary = ["and"; "or"]
-let logical_unary  = ["not"]
+let logical_unary = ["not"]
+let input_output = ["printi"]
 
 let default_context =
+  let ctxt = add_variables input_output
+                           (Function_t ([Int_t], Int_t))
+                           empty_context
+  in
   let ctxt = add_variables iarithmetic
                            (Function_t ([Int_t; Int_t], Int_t))
-                           empty_context
+                           ctxt
   in
   let ctxt = add_variables icomparison
                            (Function_t ([Int_t; Int_t], Bool_t))
